0ec6030111374830ed75dba38c1547352ee12655
Sofiastenli <144795332+Sofiastenli@users.noreply.github.com>
2023-11-28 13:15:06 +0200

    fkjjf

--- lab_3_task_3.py
+++ lab_3_task_3.py
@@ -1,14 +1,18 @@
 import numpy as np
 from lab_3_task_1 import acceleration_gravity as g 
-x0=int(input('введите начальное положение тела на ось X :'))
-y0=int(input('введите начальное положение тела на ось Y:'))
-V0=int(input('введите начальную скорость тела:'))
-t = int(input('введите время движения:'))
+x0=0
+y0=7
+V0=9
+
 t= np.arange(0,5,0.1)
 x = x0 + V0*t 
 y = y0 +V0*t-g*t**2/2
-
-print(t,x,y)
+coords=np.zeros((len(t),3))
+for i in range(0,len(t)):
+    coords[i,0]=t[i]
+for j in range(0,len(x)):
+    coords[0,j]=x[j]
+print(coords)
     
 
 
