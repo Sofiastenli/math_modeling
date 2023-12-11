754aa2f2ba15d50456501a9def1bb1f677f612ac
Sofiastenli <144795332+Sofiastenli@users.noreply.github.com>
2023-12-12 02:24:48 +0500

    rkgkr

---
+++ lab_4_task_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def average(massive):
+    s=0
+    for i in massive:
+        s=s+i
+        aver=s/len(massive)
+        print(aver)
+mass1=np.arange
+average(mass1)
---
+++ lab_4_task_3.py
@@ -0,0 +1,8 @@
+import numpy as np
+g = 10
+
+def energy(m,h,v):
+    energy = m*g*h + m*v**2/2
+    print(energy)
+
+energy(5,8,20)
