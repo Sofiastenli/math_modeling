e1640f8d534092f9182c7b8fb32970f4d9a560d4
Sofiastenli <144795332+Sofiastenli@users.noreply.github.com>
2024-03-19 14:04:05 +0200

    fjgofgj

--- lab7_task3_2.py
+++ lab7_task3_2.py
@@ -1,29 +1,28 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 from matplotlib.animation import FuncAnimation
+fig, ax = plt.subplots()
+anim_object, = plt.plot([], [], '-', lw=2)
 
 xdata, ydata = [], []
+ax.set_xlim(-20, 20)
+ax.set_ylim(-20, 20)
 
 
-def heart(t):
-    t = np.arange(0,2*np.pi, 0.01)
-    xdata.append(16*np.sin(t)**3)
-    ydata.append(13*np.cos(t) - 5*np.cos(2*t) - 2**np.cos(3*t) - np.cos(4*t))
-    ball.set_data(xdata, ydata)
-    return ball,
-
-if __name__ == '__main__': 
-    fig, ax = plt.subplots()
-    ball, = plt.plot([],[], color='r')
-
-    edge = 30
-    plt.axis('equal')
+def heart(frame):
+    xdata.append(16*np.sin(frame)**3)
+    ydata.append(13*np.cos(frame) - 5*np.cos(2*frame) - 2**np.cos(3*frame) - np.cos(4*frame))
+    anim_object.set_data(xdata, ydata)
+    return anim_object,
     
-    ax.set_xlim(-edge, edge)
-    ax.set_ylim(-edge, edge)
 
-    ani = FuncAnimation(fig,
-                        heart,
-                        frames=100,
-                        interval=40)
-    ani.save('animation_3_2.gif')
+
+    
+
+    
+
+ani = FuncAnimation(fig,
+                    heart,
+                    frames=np.arange(0,2*np.pi, 0.01),
+                    interval=40)
+ani.save('animation_3_2.gif')
--- lec_7_task3.py
+++ lec_7_task3.py
@@ -1,28 +1,27 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 from matplotlib.animation import FuncAnimation
+fig, ax = plt.subplots()
+anim_object, = plt.plot([], [], '-', lw=2)
 
 xdata, ydata = [], []
+ax.set_xlim(-20,20)
+ax.set_ylim(-20,20)
 
 
-def but(t):
-    t = np.arange(0,12*np.pi, 0.01)
-    xdata.append(np.sin(t)*(np.e**np.cos(t) - 2*np.cos(4*t) + np.sin(t/12)**5))
-    ydata.append(np.cos(t)*(np.e**np.cos(t) - 2*np.cos(4*t) + np.sin(t/12)**5))
-    ball.set_data(xdata, ydata)
-    return ball,
+def but(frame):
+    xdata.append(np.sin(frame)*(np.e**np.cos(frame) - 2*np.cos(4*frame) + np.sin(frame/12)**5))
+    ydata.append(np.cos(frame)*(np.e**np.cos(frame) - 2*np.cos(4*frame) + np.sin(frame/12)**5))
+    anim_object.set_data(xdata, ydata)
+    return anim_object,
 
-if __name__ == '__main__': 
-    fig, ax = plt.subplots()
-    ball, = plt.plot([],[], color='r')
 
-    edge = 10
-    plt.axis('equal')
-    ax.set_xlim(-edge, edge)
-    ax.set_ylim(-edge, edge)
+   
+    
+    
 
-    ani = FuncAnimation(fig,
-                        but,
-                        frames=60,
-                        interval=40)
-    ani.save('animation_3.gif')
+ani = FuncAnimation(fig,
+                    but,
+                    frames=np.arange(0,12*np.pi, 0.1),
+                    interval=100)
+ani.save('animation_3.gif')
